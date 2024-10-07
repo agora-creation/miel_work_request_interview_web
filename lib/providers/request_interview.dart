73164c189e41d711ec7e7f33c0bc2db9a031b7d6
yutashimamura <charakuri8@gmail.com>
2024-10-07 18:10:05 +0900

    20241007 commit

--- lib/providers/request_interview.dart
+++ lib/providers/request_interview.dart
@@ -1,3 +1,4 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:miel_work_request_interview_web/services/request_interview.dart';
 
@@ -7,13 +8,15 @@ class RequestInterviewProvider with ChangeNotifier {
   Future<String?> create() async {
     String? error;
     try {
-      String id = _interviewService.id();
-      _interviewService.create({
-        'id': id,
-        'approval': 0,
-        'approvedAt': DateTime.now(),
-        'approvalUsers': [],
-        'createdAt': DateTime.now(),
+      await FirebaseAuth.instance.signInAnonymously().then((value) {
+        String id = _interviewService.id();
+        _interviewService.create({
+          'id': id,
+          'approval': 0,
+          'approvedAt': DateTime.now(),
+          'approvalUsers': [],
+          'createdAt': DateTime.now(),
+        });
       });
     } catch (e) {
       error = '申込に失敗しました';
