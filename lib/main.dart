62d334a6d5cb1cb6fbbfc8c8f3e07f9ee3423d08
yutashimamura <charakuri8@gmail.com>
2024-10-07 10:53:06 +0900

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,62 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
+import 'package:miel_work_request_interview_web/common/style.dart';
+import 'package:miel_work_request_interview_web/screens/splash.dart';
+import 'package:provider/provider.dart';
+
+Future main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: const FirebaseOptions(
+        apiKey: "AIzaSyCPYXWqVx6QGgofsI2XUqMq-8V4H0hjtrg",
+        authDomain: "miel-work-project.firebaseapp.com",
+        projectId: "miel-work-project",
+        storageBucket: "miel-work-project.appspot.com",
+        messagingSenderId: "66212259980",
+        appId: "1:66212259980:web:9176778d21b0b4983933a9",
+        measurementId: "G-6Y803HEGCL"),
+  );
+  await FirebaseAuth.instance.setPersistence(Persistence.LOCAL);
+  if (FirebaseAuth.instance.currentUser == null) {
+    await Future.any([
+      FirebaseAuth.instance.userChanges().firstWhere((e) => e != null),
+      Future.delayed(const Duration(milliseconds: 3000)),
+    ]);
+  }
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MultiProvider(
+      providers: [],
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        localizationsDelegates: const [
+          GlobalMaterialLocalizations.delegate,
+          GlobalWidgetsLocalizations.delegate,
+          GlobalCupertinoLocalizations.delegate,
+        ],
+        supportedLocales: const [Locale('ja')],
+        locale: const Locale('ja'),
+        title: 'ひろめWORK - WEB申込フォーム(取材)',
+        theme: customTheme(),
+        home: const SplashController(),
+      ),
+    );
+  }
+}
+
+class SplashController extends StatelessWidget {
+  const SplashController({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SplashScreen();
+  }
+}
---
+++ lib/screens/splash.dart
@@ -0,0 +1,55 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_spinkit/flutter_spinkit.dart';
+import 'package:miel_work_request_interview_web/common/style.dart';
+import 'package:miel_work_request_interview_web/widgets/animation_background.dart';
+
+class SplashScreen extends StatelessWidget {
+  const SplashScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Stack(
+        children: [
+          AnimationBackground(),
+          Center(
+            child: SizedBox(
+              width: 400,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  Column(
+                    children: [
+                      Text(
+                        'ひろめWORK',
+                        style: TextStyle(
+                          color: kBlackColor,
+                          fontSize: 26,
+                          fontWeight: FontWeight.bold,
+                          fontFamily: 'SourceHanSansJP-Bold',
+                          letterSpacing: 4,
+                        ),
+                      ),
+                      Text(
+                        'WEBアプリ',
+                        style: TextStyle(
+                          color: kBlackColor,
+                          fontSize: 18,
+                          fontWeight: FontWeight.bold,
+                          fontFamily: 'SourceHanSansJP-Bold',
+                          letterSpacing: 3,
+                        ),
+                      ),
+                    ],
+                  ),
+                  SizedBox(height: 24),
+                  SpinKitFadingCircle(color: kBlackColor),
+                ],
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
